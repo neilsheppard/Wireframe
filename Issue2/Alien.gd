0c8384890a00c009f5d3048b812c816e5cb64e09
Neil Sheppard <developer@neilsheppard.com>
2022-08-12 13:20:22 +0100

    Functionality complete
    Sprite file reoriented
    Sprite rotation implemented
    Initial sprite rotation removed

--- Issue2/Alien.gd
+++ Issue2/Alien.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 onready var sprite = $Sprite
+onready var text = $Angle
 
 enum Controller {SHIP_PAN, SHIP_DIVE}
 var screen_size: Vector2
@@ -25,7 +26,6 @@ func _ready():
 func move():
 	ship_controller = Controller.SHIP_PAN
 	velocity = Vector2(100, 0)
-	rotation = 0
 	timer.start()
 	
 func _physics_process(delta):
@@ -40,24 +40,16 @@ func ship_controller_dive(delta):
 	position = make_individual_dive(delta_time)
 	var next_position = make_individual_dive(delta_time + epsilon)
 	
-	var y = next_position.y - position.y
-	var x = next_position.x - position.x
-	var angle = atan2(y, x)
-	rotation_degrees += angle
-#	rotation_degrees += rad2deg(position.angle_to(make_individual_dive(delta_time + epsilon)))
-	
-
-#	lerp(rotation,rotate_to.angle(),0.1)
-#	global_position.direction_to(dive_path(delta_time + epsilon)).angle() 
-#	rad2deg(dive_path(delta_time + epsilon).angle())
+	rotation_degrees = rad2deg(position.angle_to_point(next_position)) - 90
 	
 	if position.y > screen_size.y:
 		ship_controller = Controller.SHIP_PAN
 		position = start_position
-		rotation = 0
+		rotation_degrees = 0
 		timer.start()
 
 func ship_controller_pan(delta):
+	rotation_degrees = 0
 	position.x += velocity.x * delta
 	if (position.x + (sprite.texture.get_size().x / 2)) >= screen_size.x - 50:
 		velocity.x = -velocity.x
@@ -82,7 +74,6 @@ func dive_path(time):
 		100 * cos(time * 2),
 		time * 350
 	)
-	
 
 func start_dive():
 	flip_x = position.x < (screen_size.x / 2)
